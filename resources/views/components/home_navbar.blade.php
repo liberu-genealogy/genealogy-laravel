f683c5602f0dc2c4e989226391a50c3f82b9e9d8
Curtis Delicata <curtis.delicata@outlook.com>
2024-03-11 21:06:26 +0000

    Update home_navbar.blade.php

--- resources/views/components/home_navbar.blade.php
+++ resources/views/components/home_navbar.blade.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('features') }}">Features</a>
