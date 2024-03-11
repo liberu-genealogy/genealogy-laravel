9c9d4d8440ebf2d3452e629ab7b6dcfed00c247f
Curtis Delicata <curtis.delicata@outlook.com>
2024-03-11 03:13:44 +0000

    Create cashier.php

---
+++ config/cashier.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+
+return [
+'plans' => [
+    'default' => [
+        'price_id' => ENV('CASHIER_STRIPE_SUBSCRIPTION_DEFAULT_PRICE_ID'),
+        'trial_days' => 14, // Optional
+        'collect_tax_ids' => false, // Optional
+    ],
+],
