84717f857905ca9b858a30f4a0d9afdb511400fb
Curtis Delicata <curtis.delicata@outlook.com>
2024-03-14 13:42:00 +0000

    Restore navbar

--- resources/views/components/home-navbar.blade.php
+++ resources/views/components/home-navbar.blade.php
@@ -1,19 +1,43 @@
-@props(['logo'])
-<div>
-    <!-- Converted Blade component code for HomeNavBar.vue -->
-    <nav>
-        <ul>
-            <li>Menu Item 1</li>
-            <li>Menu Item 2</li>
-            <li>Menu Item 3</li>
-        </ul>
-    </nav>
-</div>
-```
-
-In the above code, the Blade component code for `HomeNavBar.vue` is converted and placed in the `resources/views/components/home_navbar.php` file.
-
-To update the references to the `logo1.svg` image, you can use the `asset()` function provided by Laravel. Replace the existing code referencing the image with the following code:
-
-```php
-<img src="{{ asset('build/assets/images/logo1.svg') }}" alt="Logo">
+<div class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Liberu Genealogy</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        More
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">About Us</a></li>
+                        <li><a class="dropdown-item" href="/contact">Contact</a></li>
+                    </ul>
+                </li>
+            </ul>
+            @if(Auth::check())
+                <span class="navbar-text">
+                    Welcome, {{ Auth::user()->name }}
+                </span>
+            @else
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/register">Register</a>
+                    </li>
+                </ul>
+            @endif
+        </div>
+    </div>
