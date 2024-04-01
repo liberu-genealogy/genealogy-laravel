4d54848972b32cac18a32dbf32f3b6ab0ba68c2b
joyudoh <joy.udoh@esolutiontec.com>
2024-04-01 22:41:41 +0100

    re-routed old livewire widgets because they were not working, and i created new widgets and added them to admin dashboard. also debugged tenecy pages. they work now

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages/'), for: 'App\\Filament\\Pages\\')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class, ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
