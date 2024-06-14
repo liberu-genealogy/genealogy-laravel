ca2def9a4f5c9bca822c4005e7382b74df35512c
Joshua Kisb <joshuakisb@gmail.com>
2024-06-14 17:14:57 +0300

    update reports

--- resources/views/livewire/daboville-report.blade.php
+++ resources/views/livewire/daboville-report.blade.php
@@ -1,35 +1,25 @@
 <div>
     <form wire:submit.prevent="generateReport">
-        <div class="flex">
-            {{ $this->form }}
-            <button type="submit" class="btn btn-primary">Generate Report</button>
+        <div class="flex items-end">
+            <div class="flex-1 pr-2">{{ $this->form }}</div>
+            <div>{{ $this->generateAction }}</div>
         </div>
     </form>
 
-    <div wire:loading>
+    <div class="flex justify-center text-center mt-3" wire:loading>
         Generating report...
     </div>
 
-    @if(!empty($reportData))
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>d'Aboville Number</th>
-                    <th>Name</th>
-                    <th>Birth Date</th>
-                    <th>Death Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($reportData as $data)
-                    <tr>
-                        <td>{{ $data['number'] }}</td>
-                        <td>{{ $data['name'] }}</td>
-                        <td>{{ $data['birth'] ?? 'N/A' }}</td>
-                        <td>{{ $data['death'] ?? 'N/A' }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    @if (!empty($reportData))
+    <div v-for="data in reportLevels">
+        <div :style="`margin: 8px 6px 8px ${data.level * 42}px; display: flex`">
+          <span :style="`width: ${(data.level * 5) + 20}px;`">{{data.label}}</span> 
+          <span>{{ data.person.firstNames }}</span>
+        </div>
+        <div v-if="data.person.spouse && data.person.children" :style="`margin: 8px 0px 16px ${(data.level * 42)}px; display: flex`">
+          <span :style="`padding: 0 8px 0px ${(data.level * 5) + 20}px`">sp</span> 
+          <span>{{ data.person.spouse.firstNames }}</span>
+        </div>
+      </div>
     @endif
 </div>
