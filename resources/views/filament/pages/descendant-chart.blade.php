9fe1c8e9b77539c653a52a6baf029252ef1378c1
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-25 06:20:55 +0000

    Upgrade

--- resources/views/filament/pages/descendant-chart.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament::page>
-    <x-filament::widgets :widgets="[
-        \App\Filament\Widgets\PedigreeChartWidget::class,
-        \App\Filament\Widgets\FanChartWidget::class,
-        \App\Filament\Widgets\DescendantChartWidget::class,
-    ]">
-    </x-filament::widgets>
-</x-filament::page>
