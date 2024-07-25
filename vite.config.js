f191c408c5dc007d356fe352d37b231d99094581
Joshua Kisb <joshuakisb@gmail.com>
2024-07-25 12:28:57 +0300

    more style changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,39 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/admin/theme.css",
+            ],
             refresh: [
                 ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
+                "app/Filament/**",
+                "app/Forms/Components/**",
+                "app/Livewire/**",
+                "app/Infolists/Components/**",
+                "app/Providers/Filament/**",
+                "app/Tables/Columns/**",
+            ],
+        }),
+
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/images/*",
+                    dest: "images",
+                },
             ],
         }),
     ],
-})
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources"),
+        },
+    },
+});
