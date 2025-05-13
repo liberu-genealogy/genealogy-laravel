c11ed690dbe5344094b0450454d1380d8bcba519
Curtis Delicata <curtis.delicata@liberu.co.uk>
2025-05-13 12:33:24 +0000

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import laravel, { refreshPaths } from 'laravel-vite-plugin'
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',
-		    'resources/css/filament/admin/theme.css'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+		        'resources/css/filament/admin/theme.css'
+            ],
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
