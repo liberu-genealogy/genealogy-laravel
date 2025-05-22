c674006aafe7a0cd374140bbd288ee1922788939
Joshua Kisb <joshuakisb@gmail.com>
2025-05-22 09:06:54 +0300

    meta token

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="application-name" content="{{ config('app.name') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="_token" content="{!! csrf_token() !!}" />
     <title>{{ config('app.name') }}</title>
     @vite('resources/css/app.css')
     @stack('styles')
