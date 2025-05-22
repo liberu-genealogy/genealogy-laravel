158e0be9235bfbe6f97e2c6c15bcbcd2e119963b
Joshua Kisb <joshuakisb@gmail.com>
2025-05-22 09:44:34 +0300

    disable csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'login',
+            'register',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
