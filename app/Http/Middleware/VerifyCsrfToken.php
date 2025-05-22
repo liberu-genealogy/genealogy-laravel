f9e07a0202fbf0a584ef5bfada4b31bf0767b481
Joshua Kisb <joshuakisb@gmail.com>
2025-05-22 18:41:08 +0300

    re-enable csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        "login",
-        "register"
+        // "login",
+        // "register"
     ];
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            'login',
-            'register',
-        ]);
+        // $middleware->validateCsrfTokens(except: [
+        //     'login',
+        //     'register',
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
