2183cc52dcec936032fada42bd9ca5d033302d1b
Curtis Delicata <genealogysoftwareuk@gmail.com>
2025-08-25 18:21:50 +0100

    Fix Dashboard.php

--- app/Filament/App/Pages/Dashboard.php
+++ app/Filament/App/Pages/Dashboard.php
@@ -6,21 +6,10 @@ use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use UnitEnum;
 use BackedEnum;
-
-class FamilyEventResource extends Resource
-{
-    protected static ?string $model = FamilyEvent::class;
-
-    protected static ?string $navigationLabel = 'Family Events';
-
-    protected static string | BackedEnum | null $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    protected static string | UnitEnum | null $navigationGroup = 'Family';
-
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
-    protected static ?string $navigationGroup = 'Dashboard';
+    protected static string | BackedEnum | null $navigationIcon = 'heroicon-o-home';
+    protected static string | UnitEnum | null $navigationGroup = 'Dashboard';
     protected static ?int $navigationSort = 1;
 
     public function getWidgets(): array
