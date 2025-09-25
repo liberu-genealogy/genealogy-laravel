eac212f8a333321cf714317d7506d709668737f1
Curtis Delicata <curtis.delicata@outlook.com>
2025-09-25 14:40:21 +0100

    Update user subscription check logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -159,7 +159,7 @@ class User extends Authenticatable implements HasDefaultTenant, HasTenants, Fila
     public function isPremium(): bool
     {
         // Consider either an active Stripe subscription or a generic user trial
-y        return $this->is_premium && ($this->subscribed('premium') || $this->onTrial());
+        return $this->is_premium && ($this->subscribed('premium') || $this->onTrial());
     }
 
     /**
