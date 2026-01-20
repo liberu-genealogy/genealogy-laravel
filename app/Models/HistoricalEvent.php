d08d5d2abfdc920c2553b2eb95fda77f8cf69568
delicatacurtis <delicatacurtis4@gmail.com>
2026-01-20 19:59:10 +0000

    Create HistoricalEvent.php

---
+++ app/Models/HistoricalEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class HistoricalEvent extends Model
+{
+    use HasFactory;
+
+    protected $table = 'historical_events';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'year',
+        'month',
+        'day',
+        'place',
+        'country',
+        'latitude',
+        'longitude',
+        'source_url',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'latitude' => 'decimal:6',
+        'longitude' => 'decimal:6',
+    ];
+
+    /**
+     * Scope events between two dates (inclusive).
+     */
+    public function scopeBetweenDates($query, $start, $end)
+    {
+        return $query->whereBetween('date', [$start, $end]);
+    }
+
+    public static function yearFromDate($date)
+    {
+        try {
+            return Carbon::parse($date)->year;
+        } catch (\Throwable $e) {
+            return null;
+        }
+    }
+}
