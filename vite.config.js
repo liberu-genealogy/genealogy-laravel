060a97cadb6832c1ffa8adae17e6084c60ff4b05
Otsemoboh Momoh <otsemoboh@yahoo.co.uk>
2026-03-15 08:39:40 +0100

    chore: save current workspace changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        cors: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         tailwindcss(),
         laravel({
             input: [
-                'resources/css/app.css', 
+                'resources/css/app.css',
                 'resources/js/app.js',
 		        'resources/css/filament/app/theme.css'
             ],
